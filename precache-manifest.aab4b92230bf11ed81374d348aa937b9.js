4d5f8d16da25cfb3a36a8a1420f85ef17307ee50
M-DeJean <dejeantech@yahoo.com>
2021-08-20 13:46:16 -0700

    Updates

---
+++ precache-manifest.aab4b92230bf11ed81374d348aa937b9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b070fd80b0d2bcf1330b1bb1d4ac9eaa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9ee6772db7a6952a652d",
+    "url": "/static/css/main.85a65b3a.chunk.css"
+  },
+  {
+    "revision": "c55daecf56d97acba9a8",
+    "url": "/static/js/2.ba9a53e1.chunk.js"
+  },
+  {
+    "revision": "0cf5ba2c00bbc0b01089c885c528fd0c",
+    "url": "/static/js/2.ba9a53e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ee6772db7a6952a652d",
+    "url": "/static/js/main.ec24e262.chunk.js"
+  },
+  {
+    "revision": "0fd04986e76745e7dc81",
+    "url": "/static/js/runtime-main.953673b5.js"
+  }
+]);
